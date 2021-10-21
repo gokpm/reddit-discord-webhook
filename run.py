b1b3ac5da718dd348c298b12318447ea9359399d
Gokul PM <contact@gokulpm.com>
2021-10-21 13:01:35 +0530

    handle connection break

--- run.py
+++ run.py
@@ -36,44 +36,45 @@ class Bot:
         return
         
     async def run(self):
+        self.reddit = asyncpraw.Reddit(
+            client_id = os.environ['CLIENT_ID'],
+            client_secret = os.environ['CLIENT_SECRET'],
+            user_agent = os.environ['USER_AGENT'])
+        self.reddit.read_only = True
+        self.sub = await self.reddit.subreddit(self.subreddit)
+        self.posts = self.sub.hot(limit = self.count)
         while True:
-            try:
-                reddit = asyncpraw.Reddit(
-                    client_id = os.environ['CLIENT_ID'],
-                    client_secret = os.environ['CLIENT_SECRET'],
-                    user_agent = os.environ['USER_AGENT'])
-                reddit.read_only = True
-                self.sub = await reddit.subreddit(self.subreddit)
-                self.posts = self.sub.hot(limit = self.count)
+            try:     
+                async for post in self.posts:
+                    if post.id in self.cache.data:
+                        continue
+                    else:
+                        self.webhook = DiscordWebhook(url=self.webhook_url)
+                        embed = DiscordEmbed(title=post.title,
+                            description=post.selftext,
+                            color = '9966cc',
+                            url = post.url)
+                        if post.is_self:
+                            pass
+                        else:
+                            embed.set_image(url=post.url)
+                        self.webhook.add_embed(embed)
+                        self.webhook.execute()
+                        self.cache.data.append(post.id)
+                        self.cache.write()
+                        await asyncio.sleep(1)
+                await self.reddit.close()
                 break
             except:
-                pass       
-        async for post in self.posts:
-            if post.id in self.cache.data:
-                continue
-            else:
-                self.cache.data.append(post.id)
-                self.cache.write()
-                self.webhook = DiscordWebhook(url=self.webhook_url)
-                embed = DiscordEmbed(title=post.title,
-                    description=post.selftext,
-                    color = '9966cc',
-                    url = post.url)
-                if post.is_self:
-                    pass
-                else:
-                    embed.set_image(url=post.url)
-                self.webhook.add_embed(embed)
-                self.webhook.execute()
-                await asyncio.sleep(1) 
+                pass
         return
         
 async def main():
     instance = []    
-    instance.append(Bot('jokes', 100, os.environ['jokes']))
-    instance.append(Bot('dankmemes', 100, os.environ['dankmemes']))
-    instance.append(Bot('python', 100, os.environ['python']))
-    instance.append(Bot('linux', 100, os.environ['linux']))
+    instance.append(Bot('jokes', 10, os.environ['jokes']))
+    instance.append(Bot('dankmemes', 10, os.environ['dankmemes']))
+    instance.append(Bot('python', 10, os.environ['python']))
+    instance.append(Bot('linux', 10, os.environ['linux']))
     tasks = []
     for i in range(len(instance)):
         tasks.append(asyncio.create_task(instance[i].run())) 
